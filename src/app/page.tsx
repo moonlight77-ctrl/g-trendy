06563d8dc875da3f96c1f2ecbf6ce8ec781956d5
moonlight77-ctrl <gordyzou@gmail.com>
2025-05-23 18:02:13 +0200

    Initial commit G-trendy

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+       <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Bienvenue sur G-trendy 👗</h1>
+    </main>
+  );
+}
